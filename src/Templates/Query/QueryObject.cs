41d43fa90377be563b51b84dffef746d220e05ca
Joao da Silva <joao.lennon@totvs.com.br>
2019-03-06 17:52:31 -0300

    Item Wrapped

--- src/Templates/Query/QueryObject.cs
+++ src/Templates/Query/QueryObject.cs
@@ -25,11 +25,22 @@ namespace Templates
         {
             get
             {
+                // Create a Property: (public int Quantity { get; set; })
+                var propertyDeclaration = SyntaxFactory.PropertyDeclaration(SyntaxFactory.ParseTypeName("IEnumerable<Item>"), "Data")
+                    .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
+                    .AddAccessorListAccessors(
+                        SyntaxFactory.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration).WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken)),
+                        SyntaxFactory.AccessorDeclaration(SyntaxKind.SetAccessorDeclaration).WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken)));
+
                 return Namespace
                     .AddUsings(Usings)
                     .AddMembers(
                         QueryClass.Inherits(TemplateConsts.IRequest(_queryResponse)))
-                    .AddMembers(ResponseClass)
+                    .AddMembers(
+                        ResponseClass
+                        .AddMembers(Item)
+                        .AddMembers(propertyDeclaration)
+                    )
                     .NormalizeWhitespace()
                     .ToFullString();
             }
@@ -39,5 +50,6 @@ namespace Templates
         private UsingDirectiveSyntax Usings => SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("System"));
         private ClassDeclarationSyntax QueryClass => SyntaxFactory.ClassDeclaration(_queryName).AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword));
         private ClassDeclarationSyntax ResponseClass => SyntaxFactory.ClassDeclaration(_queryResponse).AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword));
+        private ClassDeclarationSyntax Item => SyntaxFactory.ClassDeclaration(nameof(Item)).AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword));
     }
 }
